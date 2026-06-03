fd3d85fbf0a98b2bde473dbb2e78a641cc9aa654
Соколов Михаил <mikhaleksokolov@edu.hse.ru>
2026-06-03 22:56:06 +0300

    Реализация класса DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -120,9 +120,23 @@ class ShoppingList:
 
         for item in other._items:
             new_list._items.append(item)
-            
+
         return new_list
 
 
 class DietaryRecipe(Recipe):
-    pass
+
+    def __init__(self, title, diet_type, ingredients=None):
+        if ingredients is None:
+            ingredients = []
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio):
+        new_recipe = super().scale(ratio)
+        return DietaryRecipe(new_recipe.title, self.diet_type, new_recipe.ingredients)
+
+    def __str__(self):
+        return f"[{self.diet_type}] {super().__str__()}"
+
+

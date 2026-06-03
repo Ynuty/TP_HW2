8e39338fb933569f0738ec25db0e3948a3635fa1
Соколов Михаил <mikhaleksokolov@edu.hse.ru>
2026-06-03 21:38:14 +0300

    Добавил метод Ingridients, с внутренней реализацией метода __init__, quabtity, __str__, __repr__, __eq__

--- recipes.py
+++ recipes.py
@@ -1,13 +1,36 @@
+class Ingredient: ## 1.1 из ТЗ
 
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
 
+    @property
+    def quantity(self):
+        return self._quantity
 
-class Ingredient:
-    pass
+    @quantity.setter
+    def quantity(self, value):
+        value = float(value)
+        if value <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self._quantity = value
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}" # Мука: 500.0 г
+
+    def __repr__(self):
+        return f"Ingredient({self.name!r}, {self.quantity}, {self.unit!r})" # Ingredient('Мука', 500.0, 'г') !r для кавычек
+
+    def __eq__(self, other):
+        if isinstance(other, Ingredient):
+            return self.name == other.name and self.unit == other.unit
+        return False
 
 
 class Recipe:
     pass
-
+    
 
 class ShoppingList:
     pass
